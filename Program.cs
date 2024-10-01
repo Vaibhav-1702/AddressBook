3b7481e99ef8a842db03410955368d759db35f51
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-10-01 12:19:11 +0530

    [Vaibhav] : Creating and Adding New Contact to Address Book

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace AddressBookApp
+{
+
+
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}\nAddress: {Address}, {City}, {State} - {Zip}\nPhone: {PhoneNumber}, Email: {Email}";
+        }
+    }
+
+
+
+
+    public class AddressBook
+    {
+        public List<Contact> Contacts { get; set; }
+
+        public AddressBook()
+        {
+            Contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+            Console.WriteLine("Contact added successfully.");
+        }
+
+        public void DisplayContacts()
+        {
+            if (Contacts.Count == 0)
+            {
+                Console.WriteLine("No contacts available.");
+                return;
+            }
+
+            foreach (var contact in Contacts)
+            {
+                Console.WriteLine(contact);
+                Console.WriteLine("-------------------------");
+            }
+        }
+
+    }
+
+
+    public class AddressBookManager
+    {
+        private AddressBook addressBook;
+
+        public AddressBookManager()
+        {
+            addressBook = new AddressBook();
+        }
+
+        public void ShowMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Display Contacts");
+                Console.WriteLine("3. Exit");
+                Console.Write("Choose an option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        AddNewContact();
+                        break;
+                    case 2:
+                        addressBook.DisplayContacts();
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option, try again.");
+                        break;
+                }
+            }
+        }
+
+        public void AddNewContact()
+        {
+            var contact = GetContactDetailsFromUser();
+            addressBook.AddContact(contact);
+        }
+
+        public Contact GetContactDetailsFromUser()
+        {
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            string address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            string city = Console.ReadLine();
+            Console.Write("Enter State: ");
+            string state = Console.ReadLine();
+            Console.Write("Enter Zip: ");
+            string zip = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            return new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+
+            AddressBookManager addressBookManager = new AddressBookManager();
+            addressBookManager.ShowMenu();
+        }
+    }
+}
